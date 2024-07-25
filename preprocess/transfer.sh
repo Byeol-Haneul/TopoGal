50e880680d33578ff99c784927e55774c400b339
Jun-Young Lee <toti010@naver.com>
2024-07-25 14:10:01 +0900

    add file transferring script

---
+++ preprocess/transfer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# FOR TRANSFERRING FILES                    #
+# NOTE: WE ARE ONLY USING z=0 (snapshot 90) #
+
+source_ep="SOURCE ENDPOINT"
+dest_ep="DESTINATION ENDPOINT"
+
+source_dir="/FOF_Subfind/IllustrisTNG/LH/"
+dest_dir="/data2/jylee/topology/IllustrisTNG"
+
+rm bf.txt
+touch bf.txt
+
+for num in {0..1000}; do
+echo "LH_$num/groups_090.hdf5 data_$num.hdf5" >> bf.txt
+done
+
+mkdir -p LH_$num
+globus transfer $source_ep:$source_dir $dest_ep:$dest_dir --batch - < bf.txt
